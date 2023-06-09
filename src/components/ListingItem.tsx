677d2be815ecc1b8b31320717e87eb0fa9eb70e2
ibrahimkeyboad <ibrahimmwanga07@gamil.com>
2023-06-09 16:03:49 +0300

    fix bug in listing item

--- src/components/ListingItem.tsx
+++ src/components/ListingItem.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Listing, Reservation, User } from '@prisma/client';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { categories } from './nav-bar/Categories';
